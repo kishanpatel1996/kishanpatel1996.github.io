a31955304ab29c914bedd271a0aa342c2aa17f20
kishanpatel1996 <60364330+kishanpatel1996@users.noreply.github.com>
2020-03-04 16:27:27 -0500

    initiaal commit

--- lab_4/scripts/main.js
+++ lab_4/scripts/main.js
@@ -1,38 +1,31 @@
-
-
 let myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/firefox2.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
 }
-
-// Personalized welcome message code
 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
 function setUserName() {
-  let myName = prompt('Please enter your name.');
-  if(!myName || myName === null) {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
     setUserName();
   } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
   }
-}
 
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-}
+  myButton.onclick = function() {
+    setUserName();
+  }
 
-myButton.onclick = function() {
-  setUserName();
-}
