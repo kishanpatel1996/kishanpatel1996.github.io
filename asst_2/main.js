4b7a5f4f5864b6855049d3358f917f8c89357121
kishanpatel1996 <60364330+kishanpatel1996@users.noreply.github.com>
2020-03-30 22:17:42 -0400

    initial commit

--- asst_2/main.js
+++ asst_2/main.js
@@ -5,23 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
-const images = ["images/pic1.jpeg", "images/pic2.jpeg", "images/pic3.jpeg", "images/pic4.jpeg", "images/pic5.jpeg" ];
+const images = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.png"];
 images.forEach(image => {
     const newImage = document.createElement('img');
     newImage.setAttribute('src', image);
+    newImage.onclick = ()=>{
+        displayedImage.setAttribute('src', image);
+    }
     thumbBar.appendChild(newImage);
 
 })
-
-const all_images = document.querySelectorAll("thumb-bar img");
-all_images.forEach(image => {
-    images.onClick("click", () => {
-        let newImage = image.getAttribute("src");
-        displayedImage.setAttribute("src", newImage);
-
-    })
-})
-console.log(all_images);
 
 /* Wiring up the Darken/Lighten button */
 btn.onClick("click", ()=> {
